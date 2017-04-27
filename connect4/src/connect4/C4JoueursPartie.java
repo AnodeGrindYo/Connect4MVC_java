d3992e1511697005d4223af57209bd08a8bafb2b
adriengodoy <mr.a.godoy@gmail.com>
2017-04-27 08:53:49 +0200

    Add files via upload
    Java MVC Tic Tac Toe v1.0

---
+++ connect4/src/connect4/C4JoueursPartie.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+package connect4;
+
+public enum C4JoueursPartie 
+{
+    JOUEUR1,
+    JOUEUR2
+}
---
+++ connect4/src/connect4/Main.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package connect4;
+
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+    	C4JouerConnect4.jouerConnect4();
+    }
+}
